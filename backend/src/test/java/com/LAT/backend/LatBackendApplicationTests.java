055544e1f240a69061033b37388632d804b9298c
C.Y. Yeung <chi.yu.yeung@hva.nl>
2023-11-28 11:17:45 +0100

    new files from backend

---
+++ backend/src/test/java/com/LAT/backend/LatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LAT.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
