0633294f3fd7eec706d25837759a9a58ddb269b1
D. van Schie <dennis.van.schie@hva.nl>
2023-11-28 10:10:37 +0100

    Add spring boot

---
+++ backend/src/main/java/com/LAT/backend/LatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.LAT.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LatBackendApplication.class, args);
+	}
+
+}
