7518f03daaf47275629ee4d0435d551167a5700f
Delbert Densu <delbert.densu@hva.nl>
2023-11-29 10:30:40 +0100

    reset initial commit

---
+++ frontend/src/app/annotation-view/annotation-view.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import css from "./annotation-view.module.css"
+
+const AnnotationView = () => {
+
+    return (
+        <div>
+            <h2 className={css.title}>Annotaties</h2>
+        </div>
+    )
+}
+
+export default AnnotationView
