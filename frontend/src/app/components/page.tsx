861733c69d380df11b0006152f15cb06383cd0ae
Taner <tanerozguner@hotmail.com>
2023-12-04 13:57:04 +0100

    Update client

--- frontend/src/app/components/page.tsx
+++ frontend/src/app/components/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+/* @jsxImportSource next/client */
 import React, { useState } from 'react';
 import { Modal, Button, Dropdown, Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
