3afc6f8f735f3439089a16f6d64bce06eed14c8e
C.Y. Yeung <chi.yu.yeung@hva.nl>
2023-12-04 15:01:40 +0100

    Hanna and I have written tests

---
+++ backend/src/test/java/com/LAT/backend/AnnotationTest.java
@@ -0,0 +1,32 @@
+package com.LAT.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AnnotationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private AnnotationRepository annotationRepository;
+
+    @Test
+    public void createAnnotation() throws Exception {
+        String json = "{\"annotationText\":\"Test Annotation\"}";
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/saveAnnotation")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.annotationText").value("Test Annotation"));
+    }
+}
