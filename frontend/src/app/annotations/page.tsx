de8c99d865d43cc3a5accfe976bef6ba37e8a46e
Kevin <kevin.van.hout@hva.nl>
2023-12-07 16:24:20 +0100

    feat: add id retrieval from url

--- frontend/src/app/annotations/page.tsx
+++ frontend/src/app/annotations/page.tsx
@@ -1,12 +1,24 @@
+"use client"
 import AnnotationView from '../annotation-view/annotation-view';
 import Popup from '../annotations/comment/page';
 import '../static/annotations.css';
 import { getProjectById } from '../services/project';
 import { Project } from './project';
+import { useSearchParams } from 'next/navigation'
 
 export default async function AnnotationPage() {
 
-  const projectData = await getProjectById(3) as Project;
+  //Get id from url
+  const searchParams = useSearchParams()
+  let id: number = 1;
+
+  if (searchParams != null) {
+    const param = searchParams.get('id');
+    id = param != null ? parseInt(param) : 1;
+    if (isNaN(id)) id = 1;
+  }
+
+  const projectData = await getProjectById(id) as Project;
 
   return (
     <>
