4d3031c466ad5a835cce423a855df227e3016752
Kevin <kevin.van.hout@hva.nl>
2023-12-07 10:31:49 +0100

    feat: add api endpoint for single project by id

--- backend/src/main/java/com/LAT/backend/rest/XmlController.java
+++ backend/src/main/java/com/LAT/backend/rest/XmlController.java
@@ -5,26 +5,28 @@ import com.LAT.backend.repository.XmlRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api")
 public class XmlController {
-    
+
     @Autowired
     private XmlRepository xmlRepository;
 
     @GetMapping("/projects")
-     public Iterable<Project> getAllProjects() {
+    public Iterable<Project> getAllProjects() {
         return xmlRepository.findAll();
     }
-    
+
+    @GetMapping("/project/{projectId}")
+    public Optional<Project> getProjectById(@PathVariable Long projectId) {
+        return xmlRepository.findById(projectId);
+    }
+
     @PostMapping("/saveXml")
     public ResponseEntity<String> addProject(@RequestBody Project project) {
         xmlRepository.save(project);
