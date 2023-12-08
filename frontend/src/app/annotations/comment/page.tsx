e54bd17d5a8080201bc5308361b242a08564632d
hannatoenbreker2000 <97166864+hannatoenbreker2000@users.noreply.github.com>
2023-12-08 12:23:25 +0100

    bug: added use client string to fix pipeline error

--- frontend/src/app/annotations/comment/page.tsx
+++ frontend/src/app/annotations/comment/page.tsx
@@ -1,4 +1,4 @@
-// 'use client';
+'use client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, {useEffect, useState} from 'react';
 import {Button, Dropdown, Form, Modal} from 'react-bootstrap';
