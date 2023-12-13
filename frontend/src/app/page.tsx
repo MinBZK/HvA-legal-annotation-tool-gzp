29910edda47269057ada85b9475c1a1c21829df4
Kevin <kevin.van.hout@hva.nl>
2023-12-13 11:00:14 +0100

    chore: add extra comments

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,21 +2,22 @@
 import { FiTrash2 } from 'react-icons/fi';
 import './static/index.css';
 import { Modal, Button, Form, Alert } from 'react-bootstrap';
-import React, {useEffect, useRef, useState} from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {getProjects, uploadXML} from './services/project'
-import {Project} from "./models/project";
+import { getProjects, uploadXML } from './services/project'
+import { Project } from "./models/project";
 import { BsDownload } from "react-icons/bs";
 import Link from 'next/link';
 
 export default function Home() {
 
   const [projects, setProjects] = useState<Project[]>([]);
-  const [show, setShow] = useState(false);
-  const [showError, setShowError] = useState(false);
+  const [show, setShow] = useState(false); // State variable for controlling visibility of the upload modal
+  const [showError, setShowError] = useState(false); // State variable for managing error visibility for the upload
   const [showProjectError, setShowProjectError] = useState(false);
   const [loading, setLoading] = useState(false);
 
+  // Wrapper function to handle close and show of upload modal
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
@@ -29,75 +30,82 @@ export default function Home() {
 
   const fetchProjects = async () => {
     try {
-        setLoading(true); // Set loading to true when starting the fetch
-        const projectsData = await getProjects();
-        setProjects(projectsData);
+      setLoading(true); // Set loading to true when starting the fetch
+      const projectsData = await getProjects();
+      setProjects(projectsData);
     } catch (error) {
-        console.error('Error fetching projects:', error);
-        setShowProjectError(true);
+      console.error('Error fetching projects:', error);
+      setShowProjectError(true);
     } finally {
-        setLoading(false); // Set loading to false regardless of success or failure
+      setLoading(false); // Set loading to false regardless of success or failure
     }
   };
 
   // Handle XML Upload
   const handleXmlUpload = async () => {
+    // Check if file input reference is not null and has a non-empty value
     if (fileInputRef.current != null && fileInputRef.current["value"] != "") {
       const reader = new FileReader();
+
+      // Read the content of the first file in the file input
       reader.readAsText(fileInputRef.current["files"][0]);
+
+      // Handle the 'onload' event when the file reading is complete
       reader.onload = async (event) => {
+        // Check if the event target is not null and the result is a string
         if (event.target != null && typeof event.target.result === "string") {
           const response = await uploadXML(event.target.result);
 
+          // Check the status of the response
           if (response.status == 201) {
-            setShow(false)
+            setShow(false);
           } else {
-            setShowError(true)
+            setShowError(true);
           }
         }
-      }
+      };
     }
   };
 
   return (
-      <>
-          <div>
-              <nav className="navbar">
-                  {<div className="navbar-title">Legal Annotation Tool</div>}
-              </nav>
+    <>
+      <div>
+        <nav className="navbar">
+          {<div className="navbar-title">Legal Annotation Tool</div>}
+        </nav>
+      </div>
+      <div className="container">
+        <main className="main-content">
+          <div className="d-flex justify-content-between align-items-center mb-3">
+            <h2 className="doc-text">Documenten</h2>
+            <button
+              className="import-button"
+              onClick={handleShow}>
+              <BsDownload className="download-icon" size={20} />
+              Importeer XML
+            </button>
           </div>
-          <div className="container">
-              <main className="main-content">
-                  <div className="d-flex justify-content-between align-items-center mb-3">
-                      <h2 className="doc-text">Documenten</h2>
-                      <button
-                          className="import-button"
-                          onClick={handleShow}>
-                          <BsDownload className="download-icon" size={20}/>
-                          Importeer XML
-                      </button>
-                  </div>
 
-                  {loading && <p className="loading-message">Loading...</p>}
+          {loading && <p className="loading-message">Loading...</p>}
 
-                  <Alert show={showProjectError} variant="danger" dismissible>
-                      <Alert.Heading>Error</Alert.Heading>
-                      <p>Something went wrong</p>
-                  </Alert>
+          <Alert show={showProjectError} variant="danger" dismissible>
+            <Alert.Heading>Error</Alert.Heading>
+            <p>Something went wrong</p>
+          </Alert>
 
           <ul className="document-list">
             {projects && projects.map((project) => (
-                <li key={project.id} className="document-item">
-                    <div className="document-info">
-                        <span className="document-title">Wet {project.id}</span>
-                    </div>
-                    <div className="actions">
-                        <Link href={{ pathname: '/annotations', query: { id: project.id } }} passHref>
-                            <button className="open-button">Open project</button>
-                        </Link>
-                        <FiTrash2 className="delete-icon" />
-                    </div>
-                </li>
+              <li key={project.id} className="document-item">
+                <div className="document-info">
+                  <span className="document-title">Wet {project.id}</span>
+                </div>
+                <div className="actions">
+                  <Link href={{ pathname: '/annotations', query: { id: project.id } }} passHref>
+                    <button className="open-button">Open project</button>
+                  </Link>
+                  <FiTrash2 className="delete-icon" />
+                </div>
+              </li>
             ))}
           </ul>
 
@@ -116,9 +124,9 @@ export default function Home() {
             </Alert>
             <Form action={handleXmlUpload}>
               <input
-                  type="file"
-                  accept="text/xml"
-                  ref={fileInputRef}
+                type="file"
+                accept="text/xml"
+                ref={fileInputRef}
               />
               <Button type='submit' className='success float-end mt-3'>
                 Upload
@@ -127,6 +135,6 @@ export default function Home() {
           </Modal.Body>
         </Modal>
       </div>
-      </>
-    );
+    </>
+  );
 }
