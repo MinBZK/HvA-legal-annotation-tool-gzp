ac194c6f16b2d5ca87acb887ea0ecc51fe4e04d1
Kevin <kevin.van.hout@hva.nl>
2023-12-13 08:50:17 +0100

    test: add intercept test for xml upload

--- frontend/cypress/e2e/legal-annotation-test/xmlUpload.cy.js
+++ frontend/cypress/e2e/legal-annotation-test/xmlUpload.cy.js
@@ -17,4 +17,36 @@ describe('Open xml upload modal', () => {
         cy.get('.modal form button').should('exist');
     })
 
+    it('Successful file upload', () => {
+
+        const fileName = 'test.xml';
+        const fileType = 'text/xml';
+        const fileInput = '.modal form input';
+
+        cy.get('button.import-button').click();
+        cy.get('.modal.show').should('exist');
+        cy.upload_file(fileName, fileType, fileInput);
+        cy.get('.modal form button').click();
+        cy.intercept('POST', '/api/saveXml', {
+            statusCode: 201,
+        });
+        cy.get('.modal.show').should('not.exist');
+    });
+
+    it('Failed file upload', () => {
+
+        const fileName = 'test.xml';
+        const fileType = 'text/xml';
+        const fileInput = '.modal form input';
+
+        cy.get('button.import-button').click();
+        cy.get('.modal.show').should('exist');
+        cy.upload_file(fileName, fileType, fileInput);
+        cy.get('.modal form button').click();
+        cy.intercept('POST', '/api/saveXml', {
+            statusCode: 500,
+        });
+        cy.get('.modal.show').should('exist');
+    });
+
 })
