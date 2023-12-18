1695158586e2febd7b522e78d5f5f4c9da306cbb
hannatoenbreker2000 <97166864+hannatoenbreker2000@users.noreply.github.com>
2023-12-18 21:17:06 +0100

    feat: added backend to pipeline

--- backend/src/test/java/com/LAT/backend/LatBackendApplicationTests.java
+++ backend/src/test/java/com/LAT/backend/LatBackendApplicationTests.java
@@ -1,10 +1,25 @@
 package com.LAT.backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.Objects;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application.properties")
 class LatBackendApplicationTests {
+
+	static {
+		Dotenv dotenv = Dotenv.configure()
+				.filename(".env") // Specify the filename
+				.load();
+
+		System.setProperty("DB_URL", Objects.requireNonNull(dotenv.get("DB_URL")));
+		System.setProperty("DB_USER", Objects.requireNonNull(dotenv.get("DB_USER")));
+		System.setProperty("DB_PASS", Objects.requireNonNull(dotenv.get("DB_PASS")));
+	}
 
 	@Test
 	void contextLoads() {
