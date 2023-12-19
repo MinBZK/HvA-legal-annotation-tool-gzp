ab9385e83c8383b2da661d2af9195fb09b3d8c98
hannatoenbreker2000 <97166864+hannatoenbreker2000@users.noreply.github.com>
2023-12-19 13:13:19 +0100

    doc: added comments and cleaned up code

--- backend/src/test/java/com/LAT/backend/LatBackendApplicationTests.java
+++ backend/src/test/java/com/LAT/backend/LatBackendApplicationTests.java
@@ -3,16 +3,22 @@ package com.LAT.backend;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+		"spring.datasource.url=${db.url}",
+		"spring.datasource.username=${db.user}",
+		"spring.datasource.password=${db.pass}"
+})
 class LatBackendApplicationTests {
 
 	@BeforeAll
 	static void setupSystemProperties() {
-		// Set system properties based on GitLab CI/CD environment variables
-		System.setProperty("DB_URL", System.getenv("CI_DB_URL"));
-		System.setProperty("DB_USER", System.getenv("CI_DB_USER"));
-		System.setProperty("DB_PASS", System.getenv("CI_DB_PASS"));
+		// Set system properties based on Maven properties
+		System.setProperty("DB_URL", System.getProperty("db.url"));
+		System.setProperty("DB_USER", System.getProperty("db.user"));
+		System.setProperty("DB_PASS", System.getProperty("db.pass"));
 	}
 
 	@Test
