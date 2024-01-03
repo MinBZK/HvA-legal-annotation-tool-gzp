385fbf73a875b9f473e745d1426e5b59e4e41835
Delbert <delbert.densu@hva.nl>
2024-01-03 19:52:30 +0100

    progress

---
+++ frontend/src/app/article-selection/page.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import '../static/index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+const ArticleSelection = () => {
+
+    // const [articlePieces, setArticlePieces] = useState<any[]>([]);
+
+    return (
+        <>
+            <div>
+                <nav className="navbar">
+                    {<div className="navbar-title">Legal Annotation Tool</div>}
+                </nav>
+            </div>
+
+            {/*{articlePieces && articlePieces.map((value: any, index) => (*/}
+            {/*    <div key={index}>*/}
+            {/*        <input type={"checkbox"}/>*/}
+            {/*        <p dangerouslySetInnerHTML={{__html: value.innerHTML}}/>*/}
+            {/*    </div>*/}
+            {/*))}*/}
+
+            <div className="container-md container-sm">
+                <h1>Iuhjj</h1>
+            </div>
+        </>
+    );
+}
+
+export default ArticleSelection;
