4487b311819be3ab6839387a3ffcff5b5bdeb3b7
D. van Schie <dennis.van.schie@hva.nl>
2024-01-10 15:07:37 +0100

    Fix projectTitle type error

--- frontend/src/app/components/export-xml-button/export-xml-button.tsx
+++ frontend/src/app/components/export-xml-button/export-xml-button.tsx
@@ -6,6 +6,7 @@ import {BsDownload} from "react-icons/bs";
 
 type ExportXMLButtonProps = {
     xmlData: string; // De XML-data die geëxporteerd moet worden
+    projectTitle: string; // De titel van het project
 };
 
 const ExportXMLButton: React.FC<ExportXMLButtonProps> = ({ xmlData, projectTitle }) => {
