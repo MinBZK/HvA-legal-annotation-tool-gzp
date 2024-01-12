bb0ebf6cde1f5cdf5b8c91f2b9ca52d33323c447
Kevin <kevin.van.hout@hva.nl>
2024-01-12 10:46:07 +0100

    feat: split up article sub selection

---
+++ frontend/src/app/components/article-selection-modal/article-selection-modal.tsx
@@ -0,0 +1,95 @@
+// src/app/components/export/ExportXMLButton.tsx
+
+import React, { useState } from 'react';
+import './article-selection-modal.css';
+import { Button, Modal } from 'react-bootstrap';
+
+type articleSelectionModalProps = {
+    xmlArticles: HTMLElement[];
+    handleArticleSelect: any;
+    cancelArticleSelect: any;
+};
+
+const ArticleSelectionModal: React.FC<articleSelectionModalProps> = ({ xmlArticles, handleArticleSelect, cancelArticleSelect }) => {
+
+    const [selectedArticlesIds, setSelectedArticleIds] = useState<string[]>([]);
+    const [articleChecked, setArticleChecked] = useState<boolean[]>([]);
+
+
+    const collectSelectedArticles = (isSelectedAll?: boolean) => {
+        if (isSelectedAll) {
+            handleArticleSelect("");
+        } else {
+            const list: string[] = [];
+
+            for (let i = 0; i < xmlArticles.length; i++) {
+                if (articleChecked[i]) {
+                    list.push(xmlArticles[i].id)
+                }
+            }
+
+            if (list.length > 0) {
+                setSelectedArticleIds(list)
+                setTimeout(() => {
+                    handleArticleSelect(list);
+                }, 1000);
+            }
+        }
+
+    }
+
+    const checkHandler = (index: number) => {
+        setArticleChecked(() => {
+            const newList = [...articleChecked];
+            newList[index] = !newList[index]
+            return newList
+        })
+    }
+
+    const disableCheck: () => boolean = () => {
+        return articleChecked.includes(true);
+    };
+
+    return <>
+        <Modal show={true} className='full-screen'>
+            <Modal.Header>
+                <Modal.Title>Selecteer artikelen</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                {xmlArticles && xmlArticles.map((value, index) => (
+                    <div key={index} className={"m-1 articles"}>
+                        <label className={"d-flex align-content-center"}>
+                            <input type={"checkbox"} checked={articleChecked[index]}
+                                onChange={() => checkHandler(index)}
+                            />
+                            <div className={"article-holder"}>
+                                <div>{value.getAttribute('label')}</div>
+                                <div>{value.getElementsByTagName("kop")[0]?.getElementsByTagName("titel")[0]?.textContent}</div>
+                            </div>
+                        </label>
+                    </div>
+                ))}
+            </Modal.Body>
+            <Modal.Footer>
+                <Button className='save float-end mt-3' disabled={!disableCheck()} onClick={() => {
+                    collectSelectedArticles()
+                }}>
+                    Bevestig selectie
+                </Button>
+
+                <Button className='save float-end m-2 mt-3' onClick={() => {
+                    collectSelectedArticles(true)
+                }}>
+                    Selecteer alles
+                </Button>
+                <Button className='cancel float-end m-2 mt-3' onClick={() => {
+                    cancelArticleSelect(true);
+                }}>
+                    Annuleer
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    </>;
+};
+
+export default ArticleSelectionModal;
