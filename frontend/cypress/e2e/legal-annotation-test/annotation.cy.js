da9d12a6f27ecb71f4a38a8b0c01695e69533750
hannatoenbreker2000 <97166864+hannatoenbreker2000@users.noreply.github.com>
2024-01-24 20:50:43 +0100

    test: frontend tests annotation

--- frontend/cypress/e2e/legal-annotation-test/annotation.cy.js
+++ frontend/cypress/e2e/legal-annotation-test/annotation.cy.js
@@ -1,80 +1,251 @@
 /// <reference types="cypress" />
 
+import {Cardinality, LawClass} from "../../../src/app/models/relation";
+
 describe('Visit annotation page', () => {
-    beforeEach(() => {
-        // Bezoek de annotatiepagina
-        cy.visit('http://localhost:3000/annotations?id=1')
-    })
 
-    it('annotaties element bestaat', () => {
-        cy.get('.annolist').should('exist')
-    })
-
-    // Hanna
-
-    const openPopupAndSelectText = () => {
-        // Trigger selection event
+    function openAnnotationForm(position) {
         cy.get('p.xml-content')
-            .trigger('mousedown')
-            .then(($el) => {
-                const el = $el[0]
-                const document = el.ownerDocument
-                const range = document.createRange()
-                range.selectNodeContents(el)
-                document.getSelection().removeAllRanges(range)
-                document.getSelection().addRange(range)
-            })
-            .trigger('mouseup')
-        cy.document().trigger('selectionchange')
+            .find('al')
+            .eq(position)
+            .then(($al) => {
+                // Start mousedown event
+                cy.wrap($al).trigger('mousedown');
+
+                // Extract the text node from the <al> tag
+                const textNode = $al[0].firstChild;
+                const document = $al[0].ownerDocument;
+                const range = document.createRange();
+
+                // Select the text node
+                range.selectNodeContents(textNode);
+
+                // Add the range to the selection
+                document.getSelection().removeAllRanges();
+                document.getSelection().addRange(range);
+
+                // Trigger mouseup event
+                cy.wrap($al).trigger('mouseup', { force: true });
+            });
     }
 
-    it('Display the popup when text is selected and select a law class', () => {
-        // Load law class options from a fixture file
-        cy.fixture('lawclasses').then((lawClassOptions) => {
-            const mockLawClasses = lawClassOptions.lawClasses
-            cy.fixture('single-project').then((projectData) => {
-                const mockProjectData = projectData;
+    beforeEach('LoadXML Component', () => {
+            cy.visit('http://localhost:3000/annotations?id=1')
 
-                // Intercept the data fetching request and respond with the mock data
-                cy.intercept('GET', '/api/project/*', mockProjectData).as('getProject');
-                cy.intercept('GET', '/api/classes', mockLawClasses).as('getLawClasses');
-
-                // Wait for the data fetching to complete
-                cy.wait(['@getProject', '@getLawClasses']);
-
-                // Check if the page contains the expected elements
-                cy.get('.navbar-title').should('contain', 'Legal Annotation Tool');
-
-                openPopupAndSelectText();
-                cy.document().trigger('selectionchange');
-
-                // Wait for the popup to be visible
-                cy.get('.modal').should('be.visible');
-                cy.get('.modal-body').contains('Geselecteerde tekst: ');
-
-                cy.get('.dropdown').should('be.visible');
-                cy.get('.modal-body').contains('Notitie');
-                cy.get('.modal-body').contains('Begrip');
-                cy.get('.modal-footer').contains('Opslaan');
-                cy.get('.modal-footer').contains('Annuleer');
-                cy.get('.modal-footer').contains('Verwijder');
-
-                // Click on the dropdown toggle to open the dropdown
-                cy.get('.dropdown-toggle').click();
-
-                cy.get('.dropdown-menu').should('be.visible');
-
-                cy.get('.dropdown-menu').contains(mockLawClasses[0].name).should('exist');
-                cy.get('.dropdown-menu').contains(mockLawClasses[1].name).should('exist');
-                cy.get('.dropdown-menu').contains(mockLawClasses[2].name).should('exist');
-
-                // Click on the first law class option
-                cy.get('.dropdown-menu').contains(mockLawClasses[0].name).click();
-
-                // Verify that the dropdown toggle now displays the selected law class
-                cy.get('.dropdown-toggle').should('contain.text', mockLawClasses[0].name);
+            cy.fixture('full-example-project.json').then((content) => {
+                cy.intercept('GET', '/api/project/*', content).as('getProject');
             });
+
+            // Mock the law classes
+            cy.fixture('lawclasses.json').then((content) => {
+                cy.intercept('GET', '/api/classes', content).as('getLawClasses');
+            });
+
+            cy.fixture('full-example-project.json').then((content) => {
+                cy.intercept('POST', '/api/saveXml', {
+                    statusCode: 201,
+                    body: content
+                }).as('postXml');
+            });
+
+            cy.visit('http://localhost:3000/annotations?id=1');
+            cy.wait(['@getProject']);
+
+        openAnnotationForm(1)
+
+        cy.intercept('GET', 'api/relations/*', {
+            fixture: 'relations_rechtsbetrekking.json',
+            statusCode: 200 // Add the desired status code here
+        }).as('fetchRelations');
+
+    });
+
+    describe('Content Loaded Checks', () => {
+        it('should check if wetvorm, notitie, begrip are loaded in the component', () => {
+
+            // Check if wetvorm is loaded
+            cy.get('label[for="selectedText"]').should('exist');
+            cy.get('.dropdown').should('exist');
+
+            // Check if notitie is loaded
+            cy.get('.form-label').should('exist');
+            cy.get('.text-input').should('exist');
+
+            // Check if begrip is loaded
+            cy.get('#dropdown-basic.dropdown.dropdown-toggle.btn.btn-secondary').should('exist');
         });
     });
 
+    describe('Error Handling: Law Class Not Selected', () => {
+        it('should show an error modal when law class is not selected', () => {
+            // Do not select a law class
+
+            // Intercept the saveXml API call
+            cy.intercept('POST', '/api/saveXml').as('updateXml');
+
+            // Click on the save button
+            cy.get('.annotated-row_save__jb9BE').click();
+
+            // Check if the error modal is visible
+            cy.get('.alert').should('be.visible');
+            cy.get('.alert-heading.h4').should('include.text', 'Error');
+            cy.get('.alert p').should('include.text', 'Selecteer alstublieft een juridische klasse.');
+        });
+    });
+
+    describe('Save annotation interactions', () => {
+        it('save, show modals, button should turn green', () => {
+            // Select a law class
+            cy.get('#dropdown-basic').click(); // Click on the law class dropdown
+            cy.get('.dropdown-menu').contains('Rechtsbetrekking').click(); // Replace 'YourLawClass' with the desired law class
+
+            cy.intercept('GET', 'api/lawclasses/*', {
+                statusCode: 200,
+                body: {
+                    id:2,
+                    name:"Rechtssubject",
+                    color:"#c2e7ff",
+                    annotations:[],
+                }
+            }).as('fetchLawClass');
+
+            // Mock the saveAnnotationToBackend API call
+            cy.intercept('POST', '/api/annotations/', {
+                statusCode: 201,
+                body: {
+                    id: 123,
+                    selectedWord: "In deze wet en de daarop berustende bepalingen wordt verstaan onder:",
+                    text: "",
+                    lawClass: { name: "Rechtsbetrekking" },
+                    project: { id: 1 },
+                    term: { definition: null, reference: "In deze wet en de daarop berustende bepalingen wordt verstaan onder:" },
+                    parentAnnotation: null,
+                    relation: null,
+                    created_at: Date.now(),
+                    created_by: "Admin"
+                }
+            }).as('saveAnnotation');
+
+            cy.intercept('POST', 'api/saveXml', {
+                statusCode: 200,
+                body: { message: 'XML updated successfully' },
+            }).as('updateXml');
+
+            // Click on the save button
+            cy.get('.annotated-row_save__jb9BE').click();
+
+            // intercept save annotation
+            cy.wait('@saveAnnotation').then((interception) => {
+                // Check if the request was successful
+                expect(interception.response.statusCode).to.equal(201);
+
+                // Access the saved annotation data if needed
+                const savedAnnotation = interception.response.body;
+                // Perform assertions or continue with your test logic
+            });
+
+            cy.wait('@updateXml');
+
+            cy.wait('@fetchRelations').then((interception) => {
+                // Check if the request was successful
+                expect(interception.response.statusCode).to.equal(200);
+
+                // Access the mocked relations data if needed
+                const mockedRelations = interception.response.body;
+                // Perform assertions or continue with your test logic
+            });
+
+            // Check if relation buttons appear
+            cy.get('.relation-buttons').should('be.visible');
+
+            // // Click on a verplicht relation button
+            cy.get('.relation-buttons p:contains("Verplicht") + button').first().click();
+
+            cy.wait('@fetchLawClass').then((interception) => {
+                // Access the intercepted request and response
+                const { request, response } = interception;
+                // Perform assertions or continue with your test logic
+                expect(response.statusCode).to.equal(200);
+                // ... other assertions based on the response
+            });
+
+            cy.wait(1000)
+
+            openAnnotationForm(3)
+
+            cy.intercept('POST', '/api/annotations/', {
+                statusCode: 201,
+                body: {
+                    id: 124,
+                    selectedWord: "In deze wet en de daarop berustende bepalingen wordt verstaan onder:",
+                    text: "",
+                    lawClass: { name: "Rechtssubject" },
+                    project: { id: 1 },
+                    term: { definition: null, reference: "In deze wet en de daarop berustende bepalingen wordt verstaan onder:" },
+                    parentAnnotation: {id: 123},
+                    relation: {id: 1},
+                    created_at: Date.now(),
+                    created_by: "Admin"
+                }
+            }).as('saveSubAnnotation');
+
+            // Save subannotationdetails
+            cy.get('.annotated-row_save__jb9BE').contains('Opslaan').click();
+
+            cy.intercept('GET', 'api/annotations/children/123', {
+                statusCode: 200,
+                body: [
+                    {
+                        id: 124,
+                        selectedWord: "In deze wet en de daarop berustende bepalingen wordt verstaan onder:",
+                        text: "",
+                        lawClass: { name: "Rechtsbetrekking" },
+                        project: { id: 1 },
+                        term: { definition: null, reference: "In deze wet en de daarop berustende bepalingen wordt verstaan onder:" },
+                        parentAnnotation: {id: 123},
+                        relation: {id:1},
+                        created_at: Date.now(),
+                        created_by: "Admin"
+                    }
+                ],
+            }).as('getChildAnnotations');
+
+            cy.wait('@saveSubAnnotation').then((interception) => {
+                // Check if the request was successful
+                expect(interception.response.statusCode).to.equal(201);
+
+                // Access the saved annotation data if needed
+                const savedAnnotation = interception.response.body;
+                // Perform assertions or continue with your test logic
+            });
+
+            cy.wait('@getChildAnnotations').then((interception) => {
+                // Access the intercepted request and response
+                const { request, response } = interception;
+                expect(response.statusCode).to.equal(200);
+            });
+
+            cy.wait('@updateXml');
+
+            // Check if the relation button turns green
+            cy.get('.relation-buttons button').first().should('have.css', 'background-color', 'rgba(76, 167, 74, 0.255)');
+
+            // Click on Afronden button
+            cy.get('.annotated-row_save__jb9BE').contains('Afronden').click();
+
+            // // Check if the modal is visible
+            cy.get('.modal').should('be.visible');
+            cy.get('.modal-title').should('include.text', 'U heeft niet alle verplichte relaties gelegd');
+            cy.get('.modal-footer button:contains("Ja")').click();
+
+            cy.wait(1000)
+
+            // // Check if the modal is visible
+            cy.get('.modal').should('be.visible');
+            cy.get('.modal-title').should('include.text', 'U heeft nog ingevulde velden');
+            cy.get('.modal-footer button:contains("Ja")').click();
+
+            cy.get('.annotationInfo').should('not.exist')
+        });
+    });
 });
