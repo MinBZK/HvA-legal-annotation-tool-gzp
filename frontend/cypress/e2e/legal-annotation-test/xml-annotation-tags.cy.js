7fd5eaf4a6c15d4594f93f9dd72d797bc4abc013
D. van Schie <dennis.van.schie@hva.nl>
2024-01-24 12:00:08 +0100

    Repair old tests that verify annotations

--- frontend/cypress/e2e/legal-annotation-test/xml-annotation-tags.cy.js
+++ frontend/cypress/e2e/legal-annotation-test/xml-annotation-tags.cy.js
@@ -1,23 +1,32 @@
 describe('XML Annotation Test', () => {
 
     /**
-     * Open the popup and select all the text in the xml-content.
-     * This is needed to be able to select the text and add an annotation.
+     * Highlight the text in the xml and open the annotation creation menu.
      */
-    const openPopupAndSelectText = () => {
-
+    const selectTextAndTriggerAnnotationMenu = () => {
+        // Select all the text in the second <al> tag
         cy.get('p.xml-content')
-            .trigger('mousedown')
-            .then(($el) => {
-                const el = $el[0]
-                const document = el.ownerDocument
-                const range = document.createRange()
-                range.selectNodeContents(el)
-                document.getSelection().removeAllRanges(range)
-                document.getSelection().addRange(range)
-            })
-            .trigger('mouseup')
-        cy.document().trigger('selectionchange')
+            .find('al')
+            .eq(1)
+            .then(($al) => {
+                // Start mousedown event
+                cy.wrap($al).trigger('mousedown');
+
+                // Extract the text node from the <al> tag
+                const textNode = $al[0].firstChild;
+                const document = $al[0].ownerDocument;
+                const range = document.createRange();
+
+                // Select the text node
+                range.selectNodeContents(textNode);
+
+                // Add the range to the selection
+                document.getSelection().removeAllRanges();
+                document.getSelection().addRange(range);
+
+                // Trigger mouseup event
+                cy.wrap($al).trigger('mouseup', { force: true });
+            });
     }
 
     /**
@@ -47,80 +56,66 @@ describe('XML Annotation Test', () => {
         cy.visit('http://localhost:3000/annotations?id=1');
         cy.wait(['@getProject']);
 
-        // Select all the text in the second <al> tag
-        cy.get('p.xml-content')
-            .find('al')
-            .eq(1)
-            .then(($al) => {
-                // Start mousedown event
-                cy.wrap($al).trigger('mousedown');
+        selectTextAndTriggerAnnotationMenu();
 
-                // Extract the text node from the <al> tag
-                const textNode = $al[0].firstChild;
-                const document = $al[0].ownerDocument;
-                const range = document.createRange();
+        // get the first dropdown toggle
+        let lawClassDropdown = cy.get('.dropdown-toggle').eq(0);
 
-                // Select the text node
-                range.selectNodeContents(textNode);
+        // Press dropdown toggle and select "Variabelwaarde" as law class
+        lawClassDropdown.click();
+        cy.get('.dropdown-menu').contains('Variabelewaarde').click();
 
-                // Add the range to the selection
-                document.getSelection().removeAllRanges();
-                document.getSelection().addRange(range);
+        // Mock the post and get annotation call
+        cy.fixture('post-annotation-response.json').then((content) => {
+            cy.intercept('POST', '/api/annotations/', content).as('postAnnotation');
+            cy.intercept('GET', '/api/annotations/1', content).as('getAnnotation');
+        });
 
-                // Trigger mouseup event
-                cy.wrap($al).trigger('mouseup', { force: true });
-            });
+        // Remove the intercept of the project
+        cy.intercept('GET', '/api/project/1', (req) => {
+            req.continue()
+        });
 
+        // Click the save button. The classname of that button starts with "annotated-row_save"
+        cy.get('button[class^="annotated-row_save"]').click();
 
+        // Wait for the post annotation call to finish
+        cy.wait(['@postAnnotation']);
 
+        // Finalize the annotation by clicking the "Afronden" button
+        cy.get('button:contains("Afronden")').click();
 
-
-        // // Select the text in the xml, open the popup and store the annotation
-        // openPopupAndSelectText();
-        //
-        // // Intercept post to api/annotations
-        // cy.intercept('POST', '/api/annotations/', {
-        //     id: 1,
-        //     text: 'test',
-        //     selectedWord: 'test',
-        //     lawClass: null,
-        //     project: null,
-        //     tempId: 1
-        // }).as('postAnnotation');
-        //
-        // // press dropdown toggle and select a law class
-        // cy.get('.dropdown-toggle').click();
-        // cy.get('.dropdown-menu').contains('Rechtssubject').click();
-        //
-        // cy.get('textarea').type('Test');
-        //
-        // // Save the annotation
-        // cy.get('button:contains("Opslaan")').click();
+        // Wait for the get annotation call to finish
+        cy.wait(['@getAnnotation']);
     });
 
     it('should add annotation tags correctly', () => {
         // Arrange and Act are handled in the before each
 
         // Assert
-        // Get the added annotation tag element in the XML
-        // let annotationTag = cy.get('p.xml-content')
-        //     .children('p')
-        //     .children('annotation');
+        let annotationTag = cy.get('p.xml-content')
+            .find('annotation')
 
         // Check if the annotation tag is present
-        // annotationTag.should('have.length', 1);
-        //
-        // // Check if the annotation tag has an id attribute with value 1, which is the id of the annotation
-        // annotationTag.should('have.attr', 'id', '1');
+        annotationTag.should('have.length', 1);
+
+        // Check if the annotation tag has an id attribute with value 1, which is the id of the annotation
+        annotationTag.should('have.attr', 'id', '1');
     });
 
-    // it('Should highlight the annotated text in the correct color', () => {
-    //     // Arrange and Act are handled in the before each
-    //
-    //     // Assert if the styles are applied to the annotated text
-    //     cy.get('p.xml-content')
-    //         .children('p')
-    //         .children('annotation')
-    //         .should('have.css', 'background-color', 'rgb(194, 231, 255)');
-    // });
+    it('Should highlight the annotated text in the correct color', () => {
+        // Arrange and Act are handled in the before each
+
+        let annotationTag = cy.get('p.xml-content')
+            .find('annotation')
+
+        // Check if the annotation tag is present
+        annotationTag.should('have.length', 1);
+
+        // Check if the annotation tag has an id attribute with value 1, which is the id of the annotation
+        annotationTag.should('have.attr', 'id', '1');
+
+        // Assert if the styles are applied to the annotated text
+        annotationTag.should('have.css', 'background-color', 'rgb(255, 243, 128)');
+    });
 });
