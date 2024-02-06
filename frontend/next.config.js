868127b0258bb16fe75f6be5568c5810ff42fd35
Mehmet <onuur.s@gmail.com>
2024-02-06 14:46:49 +0100

    feat: add base path to frontend

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  basePath: process.env.NEXT_BASE_PATH,
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
